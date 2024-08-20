6f42bc6b8eba8cab62263dc0597fd23184b97c15
douglas-front <douglasjknw007@gmail.com>
2024-08-19 22:57:01 -0300

    feat: about and sectionVideo finished, and new design nav added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import About from "./layouts/About";
 import Hero from "./layouts/Hero";
 import Nav from "./layouts/Nav";
+import SectionVideo from "./layouts/SectionVideo";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <About/>
+      <SectionVideo/>
     </>
   );
 }
